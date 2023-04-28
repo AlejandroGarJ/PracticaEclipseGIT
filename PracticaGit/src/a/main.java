2d3c6afe2983984cb23767d882b803238caebb30
Alejandro Garcia Jimenez <alejgarciaj@gmail.com>
2023-04-28 12:19:46 +0200

    Añado Alumno1:

--- PracticaGit/src/a/main.java
+++ PracticaGit/src/a/main.java
@@ -5,6 +5,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Listado de alumnos 1º DAW");
+System.out.println("Alumno 1:");
 	}
 
 }
