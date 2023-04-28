2a1153827131bf95a895187414d84b41946303ac
Alejandro Garcia Jimenez <alejgarciaj@gmail.com>
2023-04-28 12:23:58 +0200

    Instancio y muestro alumno

--- PracticaGit/src/a/main.java
+++ PracticaGit/src/a/main.java
@@ -6,6 +6,10 @@ public class main {
 		// TODO Auto-generated method stub
 System.out.println("Listado de alumnos 1º DAW");
 System.out.println("Alumno 1:");
+
+
+Alumno a1=new Alumno("Perrez","Ramon","12321351");
+System.out.println(a1);
 	}
 
 }
