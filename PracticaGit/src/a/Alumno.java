6f8af4aed0d7d828680f288c2ef637165c7e44f2
Alejandro Garcia Jimenez <alejgarciaj@gmail.com>
2023-04-28 12:21:55 +0200

    Añado constructor

--- PracticaGit/src/a/Alumno.java
+++ PracticaGit/src/a/Alumno.java
@@ -4,4 +4,9 @@ public class Alumno {
 	
 	private String nombre,apellido,dni;
 
+	Alumno(String nombre, String apellido, String dni){
+		this.apellido=apellido;
+		this.nombre=nombre;
+		this.dni=dni;
+	}
 }
